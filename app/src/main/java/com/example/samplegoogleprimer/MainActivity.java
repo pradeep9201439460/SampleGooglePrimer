8074b7e5b94b39834d532245cdb20988770d8604
pradeepkumarsingh <p9201439460@gmail.com>
2020-12-25 14:07:43 +0530

    Add recycler view

--- app/src/main/java/com/example/samplegoogleprimer/MainActivity.java
+++ app/src/main/java/com/example/samplegoogleprimer/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.samplegoogleprimer;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.samplegoogleprimer.network.APIClient;
 import com.example.samplegoogleprimer.network.ApiInterface;
@@ -18,18 +21,40 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private ApiInterface apiInterface;
+    private SampleAdapter adapter;
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    private ProgressBar loadingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        doApiCall();
+    }
+
+    private void init() {
+        recyclerView = findViewById(R.id.recyclerView);
+        progressBar = findViewById(R.id.progressBar);
+        loadingView = findViewById(R.id.loadingView);
         apiInterface = APIClient.getClient().create(ApiInterface.class);
+        adapter = new SampleAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void doApiCall() {
         Call<String> call = apiInterface.getSampleData();
         call.enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
                 SampleResponse sampleResponse = new Gson().fromJson(response.body().replaceFirst("/", ""), SampleResponse.class);
-                Log.d(TAG, sampleResponse.data.get(0).text);
+                recyclerView.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+                progressBar.setMax(sampleResponse.data.size());
+                adapter.update(sampleResponse.data);
+                loadingView.setVisibility(View.GONE);
             }
 
             @Override
---
+++ app/src/main/java/com/example/samplegoogleprimer/SampleAdapter.java
@@ -0,0 +1,55 @@
+package com.example.samplegoogleprimer;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.samplegoogleprimer.pojo.SampleData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleAdapter extends RecyclerView.Adapter<SampleAdapter.SampleViewHolder> {
+
+    private List<SampleData> sampleDataList;
+
+    public SampleAdapter() {
+        sampleDataList = new ArrayList<>();
+    }
+
+    public void update(List<SampleData> sampleDataList) {
+        this.sampleDataList.clear();
+        this.sampleDataList.addAll(sampleDataList);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public SampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_google_primer_view, parent, false);
+        return new SampleViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SampleViewHolder holder, int position) {
+        holder.textView.setText(sampleDataList.get(position).text);
+    }
+
+    @Override
+    public int getItemCount() {
+        return sampleDataList.size();
+    }
+
+    public static class SampleViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public SampleViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.textView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
